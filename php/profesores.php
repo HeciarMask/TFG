54eebafc882f529ba15c8b6aa64dd05006483f16
Francisco Lopez <franheciar@gmail.com>
2023-05-31 20:55:51 +0200

    2

--- php/profesores.php
+++ php/profesores.php
@@ -6,8 +6,12 @@ $password = "aFlopez.728"; // Contraseña: aFlopez.728
 $basedatos = "barrionuevo";
 
 $conn = mysqli_connect($servername, "root", "aFlopez.728", "barrionuevo");
+$profesores = array();
 
 $sql = "SELECT * FROM usuarios WHERE tipo='profe'";
 $resul = mysqli_query($conn, $sql);
+while($fila = $resul->fetch_assoc()){
+    $profesores[] = $fila;
+}
 
-echo json_encode($resul);
+echo json_encode($profesores);
